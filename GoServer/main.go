7eaa6c3b3bbf1c1401cedf2ce721a9eecf806cac
xuanzeang <xuanze.ang@shopee.com>
2021-09-05 03:41:18 +0800

    Built GoServer with listing CRUD APIs

---
+++ GoServer/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/aaronangxz/TIC2601/controllers"
+	"github.com/aaronangxz/TIC2601/models"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"data": "hello world"})
+	})
+
+	models.ConnectDataBase()
+
+	//Available endpoints
+	r.GET("/listings", controllers.GetAllListings)
+	r.POST("/listings", controllers.CreateListing)
+	r.GET("/listings/:item_id", controllers.GetListingByItemID)
+	r.PATCH("/listings/:item_id", controllers.UpdateListing)
+	r.DELETE("/listings/:item_id")
+
+	r.Run()
+}
