5a634cd087bad700fa009f934453061a0ace1694
aaronangxz <angxze@gmail.com>
2021-10-14 03:26:41 +0800

    Added verbose logs and error handling

--- GoServer/controllers/listings/get_listings_using_filters.go
+++ GoServer/controllers/listings/get_listings_using_filters.go
@@ -1,6 +1,7 @@
 package listings
 
 import (
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
@@ -11,48 +12,57 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func ValidateGetListingsUsingFiltersRequest(c *gin.Context, input *models.GetListingsUsingFiltersRequest) {
+func ValidateGetListingsUsingFiltersRequest(c *gin.Context, input *models.GetListingsUsingFiltersRequest) error {
 	//allow nil and empty
 	if err := c.ShouldBindJSON(&input); err != nil {
 
 		if input.CategoryFilter.ItemCategory != nil && !utils.ValidateUint(input.CategoryFilter.ItemCategory) {
 			c.JSON(http.StatusBadRequest, gin.H{"Respmeta": models.NewParamErrorsResponse("item_category must be uint type.")})
-			return
+			errormsg := fmt.Sprintf("item_category must be uint. input: %v", input.CategoryFilter.ItemCategory)
+			return errors.New(errormsg)
 		}
 
 		if input.LocationFilter.Location != nil && !utils.ValidateString(input.LocationFilter.Location) {
 			c.JSON(http.StatusBadRequest, gin.H{"Respmeta": models.NewParamErrorsResponse("location must be string type.")})
-			return
+			errormsg := fmt.Sprintf("location must be string. input: %v", input.LocationFilter.Location)
+			return errors.New(errormsg)
 		}
 
 		if input.PriceFilter.MinPrice != nil && !utils.ValidateUint(input.PriceFilter.MinPrice) {
 			c.JSON(http.StatusBadRequest, gin.H{"Respmeta": models.NewParamErrorsResponse("min_price must be uint type.")})
-			return
+			errormsg := fmt.Sprintf("min_price must be uint. input: %v", input.PriceFilter.MinPrice)
+			return errors.New(errormsg)
 		}
 
 		if input.PriceFilter.MaxPrice != nil && !utils.ValidateUint(input.PriceFilter.MaxPrice) {
 			c.JSON(http.StatusBadRequest, gin.H{"Respmeta": models.NewParamErrorsResponse("max_price must be uint type.")})
-			return
+			errormsg := fmt.Sprintf("max_price must be uint. input: %v", input.PriceFilter.MaxPrice)
+			return errors.New(errormsg)
 		}
 
 		c.JSON(http.StatusBadRequest, gin.H{"Respmeta": models.NewJSONErrorResponse(err)})
-		return
+		errormsg := fmt.Sprint("JSON error: &v", err.Error())
+		return errors.New(errormsg)
 	}
+	return nil
 }
 
-func ValidateGetListingsUsingFiltersInput(c *gin.Context, input *models.GetListingsUsingFiltersRequest) {
+func ValidateGetListingsUsingFiltersInput(c *gin.Context, input *models.GetListingsUsingFiltersRequest) error {
 	//check if exists
 	if input.CategoryFilter.ItemCategory != nil && !constant.CheckListingConstant(constant.LISTING_CONSTANT_TYPE_ITEM_CATEGORY, input.GetItemCategory()) {
 		c.JSON(http.StatusBadRequest, gin.H{"Respmeta": models.NewParamErrorsResponse("unknown item_category.")})
-		return
+		errormsg := fmt.Sprintf("unknown item_category. input: %v", input.CategoryFilter.ItemCategory)
+		return errors.New(errormsg)
 	}
 
 	if input.PriceFilter.MinPrice != nil && input.PriceFilter.MaxPrice != nil {
 		if input.PriceFilter.GetMaxPrice() < input.PriceFilter.GetMinPrice() {
 			c.JSON(http.StatusBadRequest, gin.H{"Respmeta": models.NewParamErrorsResponse("min_price cannot > max_price.")})
-			return
+			errormsg := fmt.Sprintf("min_price cannot > max_price. input: min_price: %v, max_price: %v", input.PriceFilter.MinPrice, input.PriceFilter.MaxPrice)
+			return errors.New(errormsg)
 		}
 	}
+	return nil
 }
 
 func GetListingsUsingFilters(c *gin.Context) {
@@ -64,47 +74,15 @@ func GetListingsUsingFilters(c *gin.Context) {
 		priceCondition    = ""
 	)
 
-	if err := c.ShouldBindJSON(&input); err != nil {
-
-		if input.CategoryFilter.ItemCategory != nil && !utils.ValidateUint(input.CategoryFilter.ItemCategory) {
-			c.JSON(http.StatusBadRequest, gin.H{"Respmeta": models.NewParamErrorsResponse("item_category must be uint type.")})
-			return
-		}
-
-		if input.LocationFilter.Location != nil && !utils.ValidateString(input.LocationFilter.Location) {
-			c.JSON(http.StatusBadRequest, gin.H{"Respmeta": models.NewParamErrorsResponse("location must be string type.")})
-			return
-		}
-
-		if input.PriceFilter.MinPrice != nil && !utils.ValidateUint(input.PriceFilter.MinPrice) {
-			c.JSON(http.StatusBadRequest, gin.H{"Respmeta": models.NewParamErrorsResponse("min_price must be uint type.")})
-			return
-		}
-
-		if input.PriceFilter.MaxPrice != nil && !utils.ValidateUint(input.PriceFilter.MaxPrice) {
-			c.JSON(http.StatusBadRequest, gin.H{"Respmeta": models.NewParamErrorsResponse("max_price must be uint type.")})
-			return
-		}
-
-		c.JSON(http.StatusBadRequest, gin.H{"Respmeta": models.NewJSONErrorResponse(err)})
-		return
+	if err := ValidateGetListingsUsingFiltersRequest(c, &input); err != nil {
+		log.Printf("Error during ValidateGetListingsUsingFiltersRequest: %v", err.Error())
 	}
 
-	//check if exists
-	if input.CategoryFilter.ItemCategory != nil && !constant.CheckListingConstant(constant.LISTING_CONSTANT_TYPE_ITEM_CATEGORY, input.GetItemCategory()) {
-		c.JSON(http.StatusBadRequest, gin.H{"Respmeta": models.NewParamErrorsResponse("unknown item_category.")})
-		return
-	}
-
-	if input.PriceFilter.MinPrice != nil && input.PriceFilter.MaxPrice != nil {
-		if input.PriceFilter.GetMaxPrice() < input.PriceFilter.GetMinPrice() {
-			c.JSON(http.StatusBadRequest, gin.H{"Respmeta": models.NewParamErrorsResponse("min_price cannot > max_price.")})
-			return
-		}
+	if err := ValidateGetListingsUsingFiltersInput(c, &input); err != nil {
+		log.Printf("Error during ValidateGetListingsUsingFiltersInput: %v", err.Error())
 	}
 
 	//build SQL queries
-
 	//category filter
 	if input.CategoryFilter.ItemCategory != nil {
 		//else concat into query
@@ -139,12 +117,14 @@ func GetListingsUsingFilters(c *gin.Context) {
 
 	orderCondition := " ORDER BY listing_ctime DESC"
 	query := "SELECT * FROM listing_tab" + categoryCondition + locationCondition + priceCondition + orderCondition
-	log.Println(query)
+	log.Printf("Executing DB query: %v", query)
 
 	if err := models.DB.Raw(query).Scan(&listings).Error; err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"Respmeta": models.NewDBErrorResponse(err)})
+		log.Printf("Error during DB query: %v", err.Error())
 		return
 	}
 
 	c.JSON(http.StatusOK, gin.H{"Respmeta": utils.ValidateGetListingsUsingFiltersResult(listings), "Data": listings})
+	log.Printf("Successful: GetListingsUsingFilters. Returned: %v", listings)
 }
