f4267d70e495ec37b16551036a3eff4c25b70e91
aaronangxz <angxze@gmail.com>
2021-10-23 13:11:26 +0800

    Added wallet struct

---
+++ GoServer/controllers/wallet/get_user_wallet.go
@@ -0,0 +1,17 @@
+package wallet
+
+import (
+	"github.com/aaronangxz/TIC2601/models"
+	"github.com/gin-gonic/gin"
+)
+
+func GetUserWallet(c *gin.Context) {
+	var (
+		walletDetails []models.GetUserWalletRequest
+	)
+
+	//check if user exists
+
+	//join wallet_transaction to get list of transaction
+
+}
