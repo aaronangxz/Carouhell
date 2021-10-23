4dc1f587db9d7f7cc19871a243651085c6dd5092
aaronangxz <angxze@gmail.com>
2021-10-23 16:39:06 +0800

    Added GetUserWalletDetails validations

--- GoServer/controllers/wallet/get_user_wallet.go
+++ GoServer/controllers/wallet/get_user_wallet.go
@@ -2,18 +2,46 @@ package wallet
 
 import (
 	"encoding/json"
+	"errors"
+	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/aaronangxz/TIC2601/models"
+	"github.com/aaronangxz/TIC2601/utils"
 	"github.com/gin-gonic/gin"
 )
+
+func ValidateGetUserWalletDetailsRequest(c *gin.Context, input *models.GetUserWalletDetailsRequest) error {
+	if err := c.ShouldBindJSON(&input); err != nil {
+		if input.UserID == nil {
+			c.JSON(http.StatusBadRequest, gin.H{"Respmeta": models.NewParamErrorsResponse("user_id cannot be empty.")})
+			errormsg := "user_id cannot be empty"
+			return errors.New(errormsg)
+		}
+		if !utils.ValidateUint(input.UserID) {
+			c.JSON(http.StatusBadRequest, gin.H{"Respmeta": models.NewParamErrorsResponse("user_id must be uint type.")})
+			errormsg := fmt.Sprintf("user_id must be uint type. input: %v", input.GetUserID())
+			return errors.New(errormsg)
+		}
+
+		c.JSON(http.StatusBadRequest, gin.H{"Respmeta": models.NewJSONErrorResponse(err)})
+		errormsg := fmt.Sprint("JSON error: &v", err.Error())
+		return errors.New(errormsg)
+	}
+	return nil
+}
 
 func GetUserWalletDetails(c *gin.Context) {
 	var (
 		input         models.GetUserWalletDetailsRequest
 		walletDetails models.GetUserWalletDetailsResponse
 	)
+
+	if err := ValidateGetUserWalletDetailsRequest(c, &input); err != nil {
+		log.Printf("Error during ValidateGetUserWalletDetailsRequest: %v", err.Error())
+		return
+	}
 
 	if err := models.DB.Raw("SELECT * FROM wallet_tab WHERE user_id = ?", input.UserID).Scan(&walletDetails).Error; err != nil {
 		//check if user exists
