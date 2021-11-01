7b58728a9fba99edcb5a60eab3570e2d1e57ff0d
aaronangxz <angxze@gmail.com>
2021-11-01 23:38:39 +0800

    Implement redis for GetListingByItemID

--- GoServer/models/setup.go
+++ GoServer/models/setup.go
@@ -1,6 +1,7 @@
 package models
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"os"
@@ -11,13 +12,16 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/go-redis/redis/v8"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
 
 var (
-	DB       *gorm.DB
-	S3Client *session.Session
+	DB          *gorm.DB
+	S3Client    *session.Session
+	RedisClient *redis.Client
+	Ctx         = context.TODO()
 )
 
 func LoadEnv() {
@@ -65,4 +69,19 @@ func NewAWSInstance() {
 	}
 	log.Println("NewAWSInstance: S3 connection established")
 	S3Client = s3Connection
+}
+
+func NewRedis() {
+
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "redis-16881.c74.us-east-1-4.ec2.cloud.redislabs.com:16881",
+		Password: "wr2PToMVbwaGmo1gd0b9ky4hlNbnwpZz", // no password set
+		DB:       0,                                  // use default DB
+	})
+
+	if err := rdb.Ping(Ctx).Err(); err != nil {
+		log.Printf("Error while establishing Redis Client: %v", err)
+	}
+	log.Println("NewRedisClient: Redis connection established")
+	RedisClient = rdb
 }
