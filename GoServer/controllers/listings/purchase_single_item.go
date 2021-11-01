5cba07bba3e058499a5a94fddeb63a46c92486ca
aaronangxz <angxze@gmail.com>
2021-11-02 00:25:09 +0800

    added cache invalidations

--- GoServer/controllers/listings/purchase_single_item.go
+++ GoServer/controllers/listings/purchase_single_item.go
@@ -190,6 +190,11 @@ func PurchaseSingleItem(c *gin.Context) {
 	newBalance := walletHold.GetWalletBalance() - totalPrice
 	resp.WalletBalance = utils.Uint32(newBalance)
 
+	//invalidate redis
+	if err := utils.InvalidateCache(utils.GetSingleListingByUserIDCacheKey, input.GetItemID()); err != nil {
+		log.Printf("Error during InvalidateCache: %v", err.Error())
+	}
+
 	c.JSON(http.StatusOK, gin.H{"Respmeta": models.NewSuccessMessageResponse("Successfully purchased listing."), "Data": resp})
 
 	log.Printf("Successful: PurchaseSingleItem")
