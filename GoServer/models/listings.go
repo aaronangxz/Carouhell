c67dc40fb23b03d33fd2620266463ad043b8f7b4
aaronangxz <angxze@gmail.com>
2021-11-08 02:08:03 +0800

    Added is_liked for get_user_details

--- GoServer/models/listings.go
+++ GoServer/models/listings.go
@@ -290,6 +290,25 @@ type GetUserListingsResponse struct {
 	ListingCtime          int64  `json:"listing_ctime"`
 	ListingMtime          int64  `json:"listing_mtime"`
 	ListingLikes          uint32 `json:"listing_likes"`
+	IsLiked               bool   `json:"is_liked"`
+}
+
+type GetUserListingsLoggedInResponse struct {
+	LItemID               uint32 `json:"item_id"`
+	ItemName              string `json:"item_name"`
+	ItemPrice             uint32 `json:"item_price"`
+	ItemQuantity          uint32 `json:"item_quantity"`
+	ItemPurchasedQuantity uint32 `json:"item_purchasedquantity"`
+	ItemDescription       string `json:"item_description"`
+	ItemLocation          uint32 `json:"item_location"`
+	ItemStatus            uint32 `json:"item_status"`
+	ItemCategory          uint32 `json:"item_category"`
+	LSellerID             uint32 `json:"seller_id"`
+	SellerName            string `json:"seller_name"`
+	ListingCtime          int64  `json:"listing_ctime"`
+	ListingMtime          int64  `json:"listing_mtime"`
+	ListingLikes          uint32 `json:"listing_likes"`
+	IsLiked               bool   `json:"is_liked"`
 }
 
 type GetPopularListingsRequest struct {
