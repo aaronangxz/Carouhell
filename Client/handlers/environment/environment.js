b217a6a9e110be4da865bc67d2ef92ce92056e0b
aaronangxz <angxze@gmail.com>
2022-01-06 01:17:23 +0800

    Added condition in getCurrentEnv

--- Client/handlers/environment/environment.js
+++ Client/handlers/environment/environment.js
@@ -6,6 +6,16 @@ function setCurrentEnv(env) {
 }
 
 function getCurrentEnv() {
+    if (document.URL.startsWith("http://localhost")) {
+        CurrentEnv = "local"
+    } else if (document.URL.indexOf("127.0.0.1:5500") != -1) {
+        CurrentEnv = "local"
+    } else if (document.URL.indexOf("test") != -1) {
+        CurrentEnv = "test"
+    } else {
+        CurrentEnv = "live"
+    }
+
     if (CurrentEnv == "local" || CurrentEnv == "test") {
         return "carouhell-sg-api-test"
     } else {
